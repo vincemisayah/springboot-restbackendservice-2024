079ee415cd68575a4eaab4921ee529e1e8b328eb
vincemisayah <vincemisayah@gmail.com>
2024-12-12 02:42:31 -0600

    filter paid invoices update

--- src/main/java/com/fisherprinting/invoicecommissionservice/fileUpload/controller/FileUploadController.java
+++ src/main/java/com/fisherprinting/invoicecommissionservice/fileUpload/controller/FileUploadController.java
@@ -33,54 +33,6 @@ public class FileUploadController {
 //    }
 
     //localhost:1118/invoiceCommissionService/fileUpload/v1/excelFile/filterPaidInvoices
-//    @PostMapping("/excelFile/filterPaidInvoices")
-//    public ResponseEntity<?> filterPaidInvoicesFromFile(@RequestParam("empID") Integer empID, @RequestParam("file") MultipartFile file) {
-//        if(fileUploadService.processPaidInvoiceExcelFile(empID, file)){
-//            List<DTOs.PaidInvoiceInfo> shortPaid = fileUploadDao.getShortPaidInvoicesListFromBuffer(empID);
-//            List<DTOs.PaidInvoiceInfo> fullyPaid = fileUploadDao.getFullyPaidInvoicesListFromBuffer(empID);
-//            List<DTOs.PaidInvoiceInfo> overPaid = fileUploadDao.getOverPaidInvoicesListFromBuffer(empID);
-//
-//            fileUploadDao.deletePaidInvoiceDataFromBuffer(empID);
-//
-//            return ResponseEntity.ok().body(Map.of(
-//                    "ShortPaidInvoices", shortPaid,
-//                    "FullyPaidInvoices", fullyPaid,
-//                    "OverPaidInvoices", overPaid
-//            ));
-//        }
-//        return ResponseEntity.internalServerError().body(Map.of("Message", "Upload attempt failed."));
-//    }
-
-//    public record ToFilter(int empID, MultipartFile file){ }
-
-    public static class ToFilter{
-        int empID;
-        MultipartFile file;
-        public ToFilter( ){
-            super();
-            this.empID = 0;
-            this.file = null;
-        }
-        public ToFilter(int empID, MultipartFile file){ }
-    }
-//    @PostMapping("/excelFile/filterPaidInvoices")
-//    public ResponseEntity<?> filterPaidInvoicesFromFile(@RequestBody ToFilter toFilter) {
-//        if(fileUploadService.processPaidInvoiceExcelFile(toFilter.empID, toFilter.file)){
-//            List<DTOs.PaidInvoiceInfo> shortPaid = fileUploadDao.getShortPaidInvoicesListFromBuffer(toFilter.empID);
-//            List<DTOs.PaidInvoiceInfo> fullyPaid = fileUploadDao.getFullyPaidInvoicesListFromBuffer(toFilter.empID);
-//            List<DTOs.PaidInvoiceInfo> overPaid = fileUploadDao.getOverPaidInvoicesListFromBuffer(toFilter.empID);
-//
-//            fileUploadDao.deletePaidInvoiceDataFromBuffer(toFilter.empID);
-//
-//            return ResponseEntity.ok().body(Map.of(
-//                    "ShortPaidInvoices", shortPaid,
-//                    "FullyPaidInvoices", fullyPaid,
-//                    "OverPaidInvoices", overPaid
-//            ));
-//        }
-//        return ResponseEntity.internalServerError().body(Map.of("Message", "Upload attempt failed."));
-//    }
-
     @PostMapping("/excelFile/filterPaidInvoices")
     public ResponseEntity<?> filterPaidInvoicesFromFile(@RequestParam("empIDStr") String empIDStr, @RequestParam("file") MultipartFile file) {
         int empID = Integer.parseInt(empIDStr);
